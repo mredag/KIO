1ef51ee21135163cdcfdc70a1ab92839ec9ac87a
eForm Developer <developer@eform-locker.local>
2026-03-07 07:12:08 +0300

    chore: remove legacy debug scratch files

--- test_intent_api.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl -v -X POST http://localhost:3001/api/intent/classify-sexual \
-  -H "Content-Type: application/json" \
-  -H "X-API-Key: dwsQf8q0BpFWXPqMhwy2SGLG/wHIw1hKyjW8eI4Cgd8=" \
-  -d '{"messageText":"merhaba masaj fiyatlari"}'
